a9258c33f58c369173f1ee59f8bc805b1af74a29
Arif Khan <khanarif2738@gmail.com>
2024-07-26 15:04:14 +0530

    Initial commits

---
+++ src/data.js
@@ -0,0 +1,39 @@
+ const reviews = [
+    {
+        id: 1,
+        name: "Arif khan",
+        job: "Software Engineer",
+        image: "https://media.licdn.com/dms/image/D4D03AQFvWaV3-Y_C8Q/profile-displayphoto-shrink_400_400/0/1674226806129?e=1727308800&v=beta&t=_DcSJaA1GlGWbYm6FKMuGttYltdVIzk8pRZVabtm9NU",
+        text: "Hi, I'm Arif Khan, a passionate front-end developer. I specialize in creating user-friendly and visually appealing web interfaces using modern technologies like HTML, CSS, and JavaScript. My expertise extends to popular frameworks and libraries like React, which I use to build dynamic and responsive web applications.",
+    },
+    {
+        id: 2,
+        name: "Upasna Bijlani",
+        job: "Jr. Recruiter at SPECTRAFORCE TECHNOLOGIES",
+        image: "https://media.licdn.com/dms/image/D4D03AQGW--kpwmgB0g/profile-displayphoto-shrink_400_400/0/1718815496678?e=1727308800&v=beta&t=5nDZiEUixSERG9hP9McBbtgLBlL1uKIhB7l7m9jsSpY",
+        text: "Over past five years I've developed interest in programming & have done a lot of languages. I've Good communication skills.I'm still learning & looking forward for more chances to do so. I've worked on several college projects. I also love to be outdoors.",
+    },
+    {
+        id: 3,
+        name: "Tanveer Alam",
+        job: "Full-stack Developer at Birlasoft",   
+        image: "https://media.licdn.com/dms/image/D5603AQFTHDCFPlqwtg/profile-displayphoto-shrink_400_400/0/1713973038101?e=1727308800&v=beta&t=NdBBipNvQtGhsJNkzrKZ0yCEkvEe3GMqycm4yDvRMPA",
+        text: "Passionate and driven, I am a budding software developer with a keen interest in creating innovative solutions. With a solid foundation in computer science and hands-on experience in coding and development, I thrive in dynamic environments where I can continuously learn and grow.",
+    },
+    {
+        id: 4,
+        name: "Abuzaid Khalid",
+        job: "Electrical Engineer",
+        image: "https://media.licdn.com/dms/image/D5603AQGSfQi6Hf1z9A/profile-displayphoto-shrink_400_400/0/1720000322593?e=1727308800&v=beta&t=JFL0fJNeC1P5lxRHWxDuWPGnrYdwibMkrj24faHm7cw",
+        text: "My goal is to apply my knowledge to create innovative solutions and contribute to the advancement of technology in the electrical engineering field. I also have expertise in industrial software such as ACAD Electrical, AutoCAD, EPLAN, Bluebeam, Revit, and MS Office. My proficiency in lighting design software like AGI32, Dialux, and Visual Lighting is also noteworthy.",
+    }, 
+    {
+        id: 5,
+        name: "Modassir Imran",
+        job: "Full-stack Developer",
+        image: "https://media.licdn.com/dms/image/D5635AQGmRybxddDrCw/profile-framedphoto-shrink_400_400/0/1719570740994?e=1722459600&v=beta&t=MmmIw6fwQT_Abfvt_AezzADKX0xnrYGCMiNcuiLrcbU",
+        text: "I am an inquisitive, focused, and diligent developer with a passion for creating innovative frontend applications that enhance organizational efficiency and success. With a strong foundation in HTML, CSS/SCSS, JavaScript, React.js/Redux,Tialwind, C++, Java ,data structures, algorithms, and problem-solving, I am skilled in leadership and teamwork.",
+    }
+ ];
+
+ export default reviews;
